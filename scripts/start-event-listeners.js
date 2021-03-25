844e35374aa2b457b938c46f25c91ceea6d7a86f
PJColombo <paulo.colombo@pm.me>
2021-03-25 13:12:02 +0100

    start script refactored. Retry logic when connection is dropped added

--- scripts/start-event-listeners.js
+++ scripts/start-event-listeners.js
@@ -1,4 +1,43 @@
 #!/usr/bin/env node
-const { start } = require('../ethereum/task-allocation-models/round-robin/event-listener-helpers')
+const { setUpEventListeners, createJobsForMockTasks } = require('../ethereum/task-allocation-models/round-robin/event-listener-helpers')
+const { getSigner } = require('../ethereum/ethers')
+const { getRRContract } = require('../ethereum/task-allocation-models/round-robin/round-robin')
+const logger = require('../winston')
+
+const WAITING_TIME = 5000
+const MAXIMUM_RETRIES = 5
+
+function start() {
+  const cronJobs = new Map()
+  let retries = 0
+
+  logger.info('Executing events listener script...')
+
+  function retry() {
+    retries += 1
+
+    if (retries > MAXIMUM_RETRIES) {
+      logger.info(`Maximum ${MAXIMUM_RETRIES} retries exceeded:  ${retries} times tried`)
+      setTimeout(run, WAITING_TIME)
+    } else {
+      run()
+    }
+
+  }
+
+  const signer = getSigner()
+
+  signer.provider._websocket.on('close', () => retry())
+  signer.provider._websocket.on('error', error => {
+    logger.error(`Connection error: ${error}`)
+    retry()
+  })
+
+  const rrContract = getRRContract(signer)
+
+  setUpEventListeners(cronJobs, rrContract)
+
+  createJobsForMockTasks(cronJobs, rrContract)
+}
 
 start()
