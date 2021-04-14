308cd52443642cc0a0797acc4bcf4bca6bf2d4fe
Jorge Saldivar <jorgesaldivar@gmail.com>
2021-04-14 15:47:31 +0200

    Implement a function to restart contract and close then the websocket connection

--- scripts/restart-contract.js
+++ scripts/restart-contract.js
@@ -1,5 +1,25 @@
 #!/usr/bin/env node
 
-const { restartContract } = require('../ethereum/task-allocation-models/round-robin/mock-data-helpers')
+const { getRRContract } = require('../ethereum/task-allocation-models/round-robin/round-robin')
+const { getSigner } = require('../ethereum/ethers')
+
+
+const restartContract = async () => {
+    const signer = getSigner()
+
+    console.log('Opening connection...')
+    const contract = getRRContract(signer)
+    
+    console.log('Sending transaction...')
+    const txResponse = await contract.restart()
+    
+    console.log('Awaiting response...')
+    const txReceipt = await txResponse.wait()
+    
+    console.log(`Contract restarted on tx ${txReceipt.transactionHash}`)
+    
+    console.log('Closing connection...')
+    signer.provider._websocket.terminate()
+}
 
 restartContract()
