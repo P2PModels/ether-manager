042aa49f0cf7e545e41c06f7c8ae2e160239f6ec
Jorge Saldivar <jorgesaldivar@gmail.com>
2021-04-16 17:03:26 +0200

    Add documentation

--- ethereum/task-allocation-models/round-robin/mock-data-helpers.js
+++ ethereum/task-allocation-models/round-robin/mock-data-helpers.js
@@ -7,6 +7,9 @@ const { getRRContract } = require('./round-robin')
 
 const signer = getSigner()
 const contract = getRRContract(signer)
+// The gas limit (in wei) was inspired by the hive.one project. It is 
+// sufficiently high to avoid delays and the same time sufficiently 
+// low to avoid expensive transactions
 const options = { gasLimit: 850000, /* gasPrice: 20000000000 */ }
 
 async function getInitialTasksIds(tasks, maxTasks = 1) {
@@ -14,11 +17,17 @@ async function getInitialTasksIds(tasks, maxTasks = 1) {
   return tasksIds.slice(0, maxTasks)
 }
 
+/**
+ * Register users in the contract
+*/
 async function registerMockUserAccounts(userAccounts) {
   const createdAccounts = []
   for (let i = 0; i < userAccounts.length; i++) {
     try {
+      // Transform usernames given in strings to byte32, 
+      // which is the type used in the contract
       const hexUser = formatBytes32String(userAccounts[i])
+      // Call the contract's method to register the user
       const txResponse = await contract.registerUser(hexUser, options)
       const txReceipt = await txResponse.wait()
       createdAccounts.push(userAccounts[i])
@@ -39,7 +48,6 @@ async function createMockTasks(tasks) {
       const reallocationTime = reallocationTimes[i]
       const txResponse = await contract.createTask(hexId, reallocationTime, options)
       const txReceipt = await txResponse.wait()
-
       createdTasks.push(tasks[i])
       console.log(`Task ${tasks[i]} created on tx ${txReceipt.transactionHash}`)
     } catch (err) {
@@ -51,23 +59,38 @@ async function createMockTasks(tasks) {
   return createdTasks
 }
 
+/**
+ * Allocate mock tasks
+ */
 async function allocateMockTasks(tasks, userAccounts) {
   const userTaskRegistry = userAccounts.reduce((reg, currUser) => {
     reg[currUser] = 0
     return reg
   }, {})
+  
+  // Get how many tasks a user can have allocated at time, 3 by default
   const maxAllocatedTasks = await contract.MAX_ALLOCATED_TASKS()
 
   for (let i = 0; i < tasks.length; i++) {
+    // Get a random index of the user array
     let randomUser = getRandomElement(userAccounts)
+    
+    // Check if the randomly selected user has already allocated the maximum
+    // number of tasks (3 by default). If this is the case, enter into a loop that
+    // search for user that doesn't have already allocated the maximum number
+    // of tasks (3).
     while (userTaskRegistry[randomUser] >= maxAllocatedTasks) {
       randomUser = getRandomElement(userAccounts)
     }
+
+    // Increment the counter of allocated tasks of the randomly selected user
     userTaskRegistry[randomUser]++
     const hexId = formatBytes32String(tasks[i])
     const hexUser = formatBytes32String(randomUser)
     
     console.log(`Sending transaction to allocate task ${tasks[i]} to user ${randomUser}...`)
+    // The contract method allocateTask is only used here. No other 
+    // method call it
     const txResponse = await contract.allocateTask(hexId, hexUser, options)
 
     console.log('Awaiting response...')
@@ -99,12 +122,11 @@ exports.generateMockData = async () => {
   const tasksIds = await getInitialTasksIds(tasks, INITIAL_TASKS)
 
   // Register amara users
-  // const createdAccounts = await registerMockUserAccounts(users)
-  // console.log(createdAccounts)
-  // Create and allocate tasks
-  // const createdTaskIds = await createMockTasks(tasksIds)
-  // allocateMockTasks([tasksIds[1], tasksIds[2]], createdAccounts)
+  const createdAccounts = await registerMockUserAccounts(users)
+  
+  // Create tasks
+  const createdTaskIds = await createMockTasks(tasksIds)
+  
+  // Allocate tasks
   await allocateMockTasks(tasksIds, users)
-
-  // restartContract()
 }
