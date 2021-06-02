417bfe67001d1efb799a37be3308c5dc9c8754d2
Jorge Saldivar <jorgesaldivar@gmail.com>
2021-06-02 14:45:07 +0200

    Update module to include the instantiation of signer and contract

--- scripts/create-mock-data.js
+++ scripts/create-mock-data.js
@@ -1,4 +1,9 @@
 #!/usr/bin/env node
 const { generateMockData } = require('../ethereum/task-allocation-models/round-robin/mock-data-helpers')
+const { getSigner } = require('../ethereum/ethers')
+const { getRRContract } = require('../ethereum/task-allocation-models/round-robin/round-robin')
 
-generateMockData()
+const signer = getSigner()
+const contract = getRRContract(signer)
+
+generateMockData(contract)
