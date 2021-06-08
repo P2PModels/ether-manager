e662042e9f40ef87035754a484c6cc8de8691989
Jorge Saldivar <jorgesaldivar@gmail.com>
2021-06-08 14:14:37 +0200

    Update constant names

--- manager/utils.js
+++ manager/utils.js
@@ -2,28 +2,28 @@ const { Contract } = require('ethers')
 const { ethers } = require('ethers')
 const roundRobinAppAbi = require('../abis/RoundRobinApp.json')
 const { 
-    RINKEBY_ROUND_ROBIN_CONTRACT_ADDRESS, 
-    RINKEBY_SERVER_ACCOUNT_PRIVATE_KEY, 
-    INFURA_PROJECT_ID 
+    REACT_APP_RINKEBY_ROUND_ROBIN_CONTRACT_ADDRESS, 
+    REACT_APP_RINKEBY_SERVER_ACCOUNT_PRIVATE_KEY, 
+    REACT_APP_INFURA_PROJECT_ID 
 } = require('./config-manager')
 
 
 function getRRContract(signer) {
-    const contractAddress = RINKEBY_ROUND_ROBIN_CONTRACT_ADDRESS
+    const contractAddress = REACT_APP_RINKEBY_ROUND_ROBIN_CONTRACT_ADDRESS
     return new Contract(contractAddress, roundRobinAppAbi, signer)
 }
 
 function setUpSigner(provider) {
-    return new ethers.Wallet(RINKEBY_SERVER_ACCOUNT_PRIVATE_KEY, provider)
+    return new ethers.Wallet(REACT_APP_RINKEBY_SERVER_ACCOUNT_PRIVATE_KEY, provider)
 }
 
 function getAdHocSigner(network='rinkeby') {
-    return setUpSigner(new ethers.providers.InfuraProvider(network, INFURA_PROJECT_ID))
+    return setUpSigner(new ethers.providers.InfuraProvider(network, REACT_APP_INFURA_PROJECT_ID))
 }
 
 function getSigner(network='rinkeby') {
     // Create a websocket to Ethereum
-    const provider = ethers.providers.InfuraProvider.getWebSocketProvider(network, INFURA_PROJECT_ID)
+    const provider = ethers.providers.InfuraProvider.getWebSocketProvider(network, REACT_APP_INFURA_PROJECT_ID)
     return setUpSigner(provider)
 }
 
